4f49ed364eee8bb2f79f2bfb0317c102ccc2a11c
Fraks51 <semzhuv@yandex.ru>
2021-02-14 23:34:36 +0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Ros_Turtlesim
 launch a turtlesim with red background
+
+To launch, type in console:
+
+''' ros2 launch launch/turtlesim\_mimic\_launch.py '''
---
+++ launch/turtlesim_mimic_launch.py
@@ -0,0 +1,21 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    node = Node(
+            package='turtlesim',
+            namespace='turtlesim1',
+            executable='turtlesim_node',
+            name='sim',
+            parameters=[
+                {
+                 'background_b': 0,
+                 'background_g': 0,
+                 'background_r': 255
+                 }
+            ]
+        )
+    node
+    return LaunchDescription([
+        node
+    ])
